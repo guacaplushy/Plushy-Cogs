31a24ac9990fb43214b2d581e7696ea7b6bf0470
Tom E <guacaplushy@icloud.com>
2021-09-06 08:44:37 -0400

    Update __init__.py

--- ErrorMessage/__init__.py
+++ ErrorMessage/__init__.py
@@ -1,8 +1,8 @@
 from redbot.core import commands
 from redbot.core import Config
 
-class MainCog(commands.Cog):
-  def __init__(self):
+class ErrorMessage(commands.Cog):
+  def __init__(self, bot : commands.Bot):
     self.config = Config.get_conf(self, identifier=835962021)
     default_global = {
       "message": "An error occured: {error}",
@@ -12,3 +12,6 @@ class MainCog(commands.Cog):
   @commands.Cog.listener()
   async def on_command_error(self, ctx, error):
     await ctx.send(self.config.message().replace("{error}", str(error)))
+
+def setup(bot : commands.Bot):
+  bot.add_cog(ErrorMessage(bot=bot))
